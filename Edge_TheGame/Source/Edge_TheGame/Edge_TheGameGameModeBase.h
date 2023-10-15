ba3cb1cca0330ac8b5dc22109741b4e8897f3ec0
Kevigan <74367343+Kevigan@users.noreply.github.com>
2023-10-16 01:11:50 +0200

    init game

---
+++ Edge_TheGame/Source/Edge_TheGame/Edge_TheGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Edge_TheGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EDGE_THEGAME_API AEdge_TheGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
